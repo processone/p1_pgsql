67e23a3291811bc485924bb70df6a1cf322fc776
Badlop <badlop@process-one.net>
2017-01-09 17:03:49 +0100

    Update copyright year (processone/ejabberd#1442)

--- src/pgsql_sup.erl
+++ src/pgsql_sup.erl
@@ -5,7 +5,7 @@
 %%% Created : 15 May 2013 by Evgeniy Khramtsov <ekhramtsov@process-one.net>
 %%%
 %%%
-%%% p1_pgsql, Copyright (C) 2002-2015   ProcessOne
+%%% p1_pgsql, Copyright (C) 2002-2017   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
