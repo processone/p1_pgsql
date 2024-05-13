bdb3ffa38bc410d4b2de5e9adee563a3b05b8f3d
Badlop <badlop@process-one.net>
2024-05-13 15:15:56 +0200

    Update copyright year to 2024

--- src/pgsql_sasl.erl
+++ src/pgsql_sasl.erl
@@ -1,7 +1,7 @@
 %%%-------------------------------------------------------------------
 %%% @author Alexey Shchepin <alexey@process-one.net>
 %%%
-%%% Copyright (C) 2002-2021 ProcessOne, SARL. All Rights Reserved.
+%%% Copyright (C) 2002-2024 ProcessOne, SARL. All Rights Reserved.
 %%%
 %%% Licensed under the Apache License, Version 2.0 (the "License");
 %%% you may not use this file except in compliance with the License.
